56ec36e0b0f6aac1d65f0a749d5e76976d05d59f
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2021-02-11 17:17:14 +0100

    docker-compose with database example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+  }
+);
+
+class User extends Model {}
+User.init(
+  {
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE,
+  },
+  { sequelize, modelName: "user" }
+);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res, next) => {
+  await sequelize.sync();
+  const jane = await User.create({
+    username: "janedoe",
+    birthday: new Date(1980, 6, 20),
+  });
+  res.json(jane);
 });
 
 module.exports = router;
